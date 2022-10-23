a339a066630cb258202426ccbdf65806d8f160e4
jinng1 <72145074+jinng1@users.noreply.github.com>
2022-10-23 15:17:10 +0800

    Changed sessions to cookie-based

--- healthcaretether/healthcaretether/settings.py
+++ healthcaretether/healthcaretether/settings.py
@@ -149,4 +149,8 @@ LOGIN_REDIRECT_URL = "/"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
-
+#Session settings
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+SESSION_COOKIE_AGE = 43200 #12 hours of active use
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
